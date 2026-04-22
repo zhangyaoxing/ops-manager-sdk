5f17fe81115d16569e931af521e30b19d40d84a9
Yaoxing <yaoxing.zhang@gmail.com>
2026-04-22 09:50:38 +0200

    - fix: data class initialization always has to provide values #9

--- tests/test_projects_resource.py
+++ tests/test_projects_resource.py
@@ -3,8 +3,11 @@ from tests.shared import get_client
 
 def test_projects_resource() -> None:
     client = get_client()
-    projects = client.projects_resource.get_all(query_params=None)
-    assert isinstance(projects, list)
+    query_params = client.projects_resource.GetAllQueryParams()
+    query_params.page_num = 1
+    query_params.items_per_page = 10
+    projects = client.projects_resource.get_all(query_params=query_params)
+    assert isinstance(projects["results"], list)
 
 
 def test_projects_resource_add() -> None:
@@ -15,4 +18,4 @@ def test_projects_resource_add() -> None:
         org_id="698117018b47f47002806d04",
     )
     new_project = client.projects_resource.create(query_params=None, body_params=body_params)
-    assert new_project["name"] == project_name
+    assert new_project["result"]["name"] == project_name
